f6fab214d22a5c9e4590a82eb2c9bf68171fc9a4
Le Xuan Hieu <hieulexuan629@gmail.com>
2021-05-08 17:32:56 +0700

    new weather angular project

--- intro2angular/src/app/app.component.ts
+++ intro2angular/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { UiService } from './service/ui.service';
+import { FbService } from './service/fb.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'intro2angular';
+export class AppComponent implements OnInit, OnDestroy {
+  showMenu = false;
+  darkModeActive!: boolean;
+
+  userEmail = '';
+
+  sub1: any;
+
+  constructor(
+    public ui: UiService,
+    public fb: FbService, 
+    public router: Router, ) {
+  }
+
+  ngOnInit() {
+    this.sub1 = this.ui.darkModeState.subscribe((value) => {
+      this.darkModeActive = value;
+    });
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.ui.darkModeState.next(!this.darkModeActive);
+  }
+
+  ngOnDestroy() {
+
+  }
 }
--- intro2angular/src/app/app.module.ts
+++ intro2angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { CardComponent } from './card/card.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherCardComponent,
+    CardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
